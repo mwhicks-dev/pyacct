380fef43a140f7bd2cb5bd054401dd15dbf0eae5
Mason <mason@hicksm.dev>
2024-08-03 16:56:39 -0400

    Implemented database schemas

---
+++ model/account.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Column, Integer, ForeignKey, String
+
+from persistence.database import Base
+
+class Account(Base):
+    __tablename__ = "accounts"
+
+    id = Column(Integer, primary_key=True)
+
+class Username(Base):
+    __tablename__ = "usernames"
+
+    id = Column(Integer, ForeignKey("accounts.id"), primary_key=True)
+    username = Column(String, unique=True, index=True)
+
+class Password(Base):
+    __tablename__ = "passwords"
+
+    id = Column(Integer, ForeignKey("accounts.id"), primary_key=True)
+    password = Column(String)
---
+++ model/session.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, ForeignKey, Uuid, DateTime, Integer, PrimaryKeyConstraint
+
+from persistence.database import Base
+
+class Session(Base):
+    __tablename__ = "sessions"
+
+    id = Column(Uuid, primary_key=True)
+    created_time = Column(DateTime)
+    used_time = Column(DateTime)
+
+class AccountSession(Base):
+    __tablename__ = "account_sessions"
+
+    session_id = Column(Uuid, ForeignKey("sessions.id"))
+    account_id = Column(Integer, ForeignKey("accounts.id"))
+
+    # ref: https://stackoverflow.com/questions/10059345/sqlalchemy-unique-across-multiple-columns
+    __table_args__ = (PrimaryKeyConstraint("session_id", "account_id"))
