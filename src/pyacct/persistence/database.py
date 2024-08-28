a3e2ce5e9a5a30227c3c4e18157ff2bf2766ef29
Mason <mason@hicksm.dev>
2024-08-28 10:49:42 -0400

    Hopefully, last fix to database config access

--- src/pyacct/persistence/database.py
+++ src/pyacct/persistence/database.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-with open("../config/config.json", "r") as f:
+with open("config/config.json", "r") as f:
     config = json.load(f)
     engine = create_engine(config['sqlalchemy_url'])
 
