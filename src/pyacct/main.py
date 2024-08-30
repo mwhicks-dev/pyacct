8d6ee4571874a4640113319e4111a77d8b3437f4
Mason <mason@hicksm.dev>
2024-08-30 15:36:53 -0400

    Extended configuration with runtime attributes #10

--- src/pyacct/main.py
+++ src/pyacct/main.py
@@ -1,10 +1,12 @@
 import os
+import json
 
 import uvicorn
 
 from pyacct_token_validator import PyacctTokenValidator
 from persistence.database import Base, engine
 import persistence.session
+from persistence.attribute import AttributeService
 from api import AccountRouter, SessionRouter
 
 from fastapi import FastAPI
@@ -12,6 +14,19 @@ from fastapi import FastAPI
 PYACCT_PORT = os.environ.get("PYACCT_PORT", 8000)
 
 Base.metadata.create_all(bind=engine)
+
+# create all attributes
+with open("config/config.json", "r") as fp:
+    config = json.load(fp)
+
+for attribute in config['attributes']:
+    # update attr will create new attribute if existing not found
+    AttributeService.update_attribute(
+        key=attribute['key'],
+        required=attribute['required'],
+        unique=attribute['unique'],
+        sensitive=attribute['sensitive']
+    )
 
 persistence.session.token_validation = PyacctTokenValidator()
 
